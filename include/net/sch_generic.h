rockchip-linux/kernel

commit 81ce24cd24a9ed540fd772d57f6e3ac5d3c80ae7

net: Properly annotate the try-lock for the seqlock
In patch
   ("net/Qdisc: use a seqlock instead seqcount")

the seqcount has been replaced with a seqlock to allow to reader to
boost the preempted writer.
The try_write_seqlock() acquired the lock with a try-lock but the
seqcount annotation was "lock".

Opencode write_seqcount_t_begin() and use the try-lock annotation for
lockdep.

Reported-by: Mike Galbraith <efault@gmx.de>
Cc: stable-rt@vger.kernel.org
Signed-off-by: Sebastian Andrzej Siewior <bigeasy@linutronix.de>

include/net/sch_generic.h
@@ -171,8 +171,16 @@ static inline bool qdisc_run_begin(struct Qdisc *qdisc)
 		return false;
 	}
 #ifdef CONFIG_PREEMPT_RT
-	if (try_write_seqlock(&qdisc->running))
+	if (spin_trylock(&qdisc->running.lock)) {
+		seqcount_t *s = &qdisc->running.seqcount.seqcount;
+		/*
+		 * Variant of write_seqcount_t_begin() telling lockdep that a
+		 * trylock was attempted.
+		 */
+		raw_write_seqcount_t_begin(s);
+		seqcount_acquire(&s->dep_map, 0, 1, _RET_IP_);
 		return true;
+	}
 	return false;
 #else
 	/* Variant of write_seqcount_begin() telling lockdep a trylock

include/net/net_seq_lock.h
@@ -6,15 +6,6 @@
 # define net_seq_begin(__r)		read_seqbegin(__r)
 # define net_seq_retry(__r, __s)	read_seqretry(__r, __s)
 
-static inline int try_write_seqlock(seqlock_t *sl)
-{
-	if (spin_trylock(&sl->lock)) {
-		write_seqcount_begin(&sl->seqcount);
-		return 1;
-	}
-	return 0;
-}
-
 #else
 # define net_seqlock_t			seqcount_t
 # define net_seq_begin(__r)		read_seqcount_begin(__r)

yasirykhan/linux-stable-rt-micro

commit 3b5b679fc12ca9aa310862d5f0a6e23d6151c6a4

net: replace raw_write_seqcount_t_begin by do_raw_write_seqcount_begin
Commit a8dd21118b0fa ("seqlock: Prefix internal seqcount_t-only macros with a
"do_""), among other changes, renames the function raw_write_seqcount_t_begin
to do_raw_write_seqcount_begin. That commit did not cover the changes from
the RT-only commit 81ce24cd24a9e ("net: Properly annotate the try-lock for the
seqlock"), which are fixed here.

Fixes: 81ce24cd24a9e ("net: Properly annotate the try-lock for the seqlock")
Signed-off-by: Luis Claudio R. Goncalves <lgoncalv@redhat.com>

include/net/sch_generic.h
@@ -195,7 +195,7 @@ nolock_empty:
 		 * Variant of write_seqcount_t_begin() telling lockdep that a
 		 * trylock was attempted.
 		 */
-		raw_write_seqcount_t_begin(s);
+		do_raw_write_seqcount_begin(s);
 		seqcount_acquire(&s->dep_map, 0, 1, _RET_IP_);
 		return true;
 	}